d4e1f61be165077cf9c452a19fb3d9e8a87979f1
Owen Sheehan <owensheehan@gmail.com>
2019-09-29 22:32:43 +0100

    PPSN calculator php

---
+++ README.md
@@ -0,0 +1,1 @@
+PPSN Calculator PHP
---
+++ ppsn.php
@@ -0,0 +1,22 @@
+<! DOCTYPE HTML>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>good_ppsn</title>
+    </head>
+    <body>
+        <header>
+            <h1>Loading Results</h1>
+        </header>
+        <main>
+            <p>hello
+            <?php
+            $ppsn = $_POST['ppsn'];
+            echo $ppsn;
+            print "hello";
+                
+            ?>
+            </p>
+        </main>
+    </body>
+</html>
